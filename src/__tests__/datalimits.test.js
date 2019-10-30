50c97237fb463e687ce2fdfa56c4c99f88c7b02f
jad <jadesrochers@gmail.com>
2019-10-30 16:13:37 -0400

    added datalimits hook to this library

---
+++ src/__tests__/datalimits.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow } from '../../enzyme';
+import { useLimits } from '../datalimits'
+import { HookWrapper } from '../hookwrappers'
+
+describe('useLimits tests', () => {
+  test('Test raw and scaled limit setting', () => {
+
+    // the wrapping function for the hook is so that args can be 
+    // passed to initialize if needed. Did not need to here.
+    let xscale = { invert: (n) => n*2 }
+    let yscale = { invert: (n) => n/2 }
+    let wrapper = shallow(<HookWrapper hook={() => useLimits()}  />) 
+    let hook = wrapper.find('div').props().hook;
+
+    expect(hook.xlimits).toEqual({min:0, max:0})
+    expect(hook.ylimits).toEqual({min:0, max:0})
+
+    hook.setRawLims('x',[15, 45])
+    hook.setRawLims('y',[5, 30])
+
+    hook = wrapper.find('div').props().hook
+    expect(hook.xlimits).toEqual({min:15, max:45})
+    expect(hook.ylimits).toEqual({min:5, max:30})
+
+    hook.setXscale(() => xscale)
+    hook.setYscale(() => yscale)
+
+    hook = wrapper.find('div').props().hook
+    hook.setLimits('x', [10, 30])
+    hook.setLimits('y', [50, 200])
+
+    hook = wrapper.find('div').props().hook
+    expect(hook.xlimits).toEqual({min:20, max:60})
+    expect(hook.ylimits).toEqual({min:25, max:100})
+
+  });
+})
---
+++ src/datalimits.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import * as R from 'ramda';
+
+
+// The limit object is designed to be passed to a Component that 
+// is selecting data and one that is displaying it to configure 
+// the data that shows up.
+const useLimits = () => {
+  const [xlimits, setxlimits] = useState({min:0, max:0})
+  const [ylimits, setylimits] = useState({min:0, max:0})
+  // The scale functions come from d3-scale; scale.invert()
+  // gets the original values, which are used to check data against limits.
+  const [xscale, setXscale] = useState(() => { let invert = n => n; return {invert: invert} })
+  const [yscale, setYscale] = useState(() => { let invert = n => n; return {invert: invert} })
+
+  const setLimits = R.curry((which, maxmin) => {
+    let vals = R.values(maxmin)
+    let scaled, scale, setlims
+    if(R.equals(which)('x')){
+      scale = xscale; setlims = setxlimits
+    }else{
+      scale = yscale; setlims = setylimits
+    }
+    scaled = R.map(scale.invert)(vals)
+    setlims({min: R.min(...scaled), max: R.max(...scaled)})
+  })
+
+  const setRawLims = (which, maxmin) => {
+    let min = R.min(...maxmin), max = R.max(...maxmin)
+    if(R.equals(which)('x')){
+      setxlimits({min: min, max: max})
+    }else{
+      setylimits({min: min, max: max})
+    }
+  }
+
+  return { setLimits, setRawLims, setXscale, setYscale, xlimits, ylimits }
+}
+
+export { useLimits }
