eb051399f67ffd10fc113dcd63c37ea8431d7654
jad <jadesrochers@gmail.com>
2020-03-14 03:53:54 -0400

    Update packages for min bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const path = require('path');
 // This config just sets a bundle type (umd)
 // and makes sure everything gets run through babel loader.
 // Does not need CSS loader because the library has no CSS.
+// IMPORTANT: even though I have React correctly set as a dev/peerdependency,
+// Webpack will still include a ref to it, so you need the externals:
+// statement for sure.
 const config = {
   entry: './src/index.js',
 
@@ -36,7 +39,12 @@ const config = {
   },
 
   externals: {
-    'react': 'react',
+    'react': {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
     'react-dom': 'react-dom',
     'reactDOM': 'react-dom',
   },
