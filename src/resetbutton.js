7a198a9b432bcca4377046f25e46450ba0e4cb8c
jad <jadesrochers@gmail.com>
2020-03-25 22:46:12 -0400

    package updates, comment/format fixes

--- src/resetbutton.js
+++ src/resetbutton.js
@@ -25,27 +25,9 @@ const baseStyle = css`
   font-size: 0.8em;
 `;
 
-// The Reset FAIL issue:
-// This is where the button and selection disappears, but no reset is done.
-// I think I finally figured out why it happens; there are two cases
-// 1. There is a click, drag that leaves the reset button.
-//    In this case the button gets highlighted, but the action does 
-//    not fire because the drag takes the click out of the button
-// 2. There is a click with slight movement. If the movement is enough
-//    to select any space it works, but if it is very slight, then the 
-//    button and box disappear, and the reset action does not fire.
-//    NOTE: if you mousedown, slightly shift, the button is gone. But, if
-//    you keep shifting it re-appears and works again, so this is some
-//    edge case with a tiny bit of movement, but not a drag that selects
-//    any area because that seems to re-ectivate it.
-// FURTHER NOTE: I can mostly confirm that any selection in the x direction
-// (> 0) makes it so this works even if you drag, but that if selectx is 0,
-// it doesn't, so slight y movement is what causes this.
-// y direction drags are what breaks this.
-// THE FIX: Figure out how to make it appear when there is an x selection, 
-// but not disappear unless it gets clicked.
-
 // Expects a limitHook and resets it to a large range
+// Keeps it visible when mouse is down to prevent slight movements
+// during the click from making it disappear which will make the click fail.
 const ResetButton = (props) => {
   /* console.log('ResetButton props: ',props) */
   return(
